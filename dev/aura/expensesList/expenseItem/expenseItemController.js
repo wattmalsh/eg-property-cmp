cd739e1b3d9b28b606d1ca290283464d56c604e6
Matt Walsh <walsh.matt.t@gmail.com>
2017-09-19 01:25:10 -0700

    Refactor directory structure to deploy/retrieve/undeploy

--- dev/aura/expensesList/expenseItem/expenseItemController.js
+++
@@ -1,9 +0,0 @@
-// expenseItemController.js
-({
-  clickReimbursed: function(component, event, helper) {
-    var expense = component.get("v.expense");
-    var updateEvent = component.getEvent("updateExpense"); // create a custom event named updateExpense
-    updateEvent.setParams({ "expense": expense }); // package expense into the event
-    updateEvent.fire();
-  },
-})
